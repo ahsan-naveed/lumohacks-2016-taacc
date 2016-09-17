f89b33425560414681ba7bb07e74525d2db1589e
amirfaraji <ahadjifa@sfu.ca>
2016-09-17 16:28:52 -0700

    getAccelData

--- src/c/main.c
+++ src/c/main.c
@@ -1,77 +1,19 @@
 #include <pebble.h>
+#include "getAccelData.c"
 
-static Window *s_main_window;
-static TextLayer *s_time_layer;
+static Window *s_main_window;//Pointer to new pebble watch window
 
-static void update_time(){
-  //get a time structure
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
+static void init(Window *window){
   
-  //Write current hours and minutes into a buffer
-  static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
-  
-  //Display this onto the TextLayer
-  text_layer_set_text(s_time_layer, s_buffer);
 }
 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
-  update_time();
-}
-
-static void main_window_load(Window *window){
-  //Get information about the Window
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+static void deinit(Window *window){
   
-  //Create the text layer with specified bounds
-  s_time_layer = text_layer_create(
-    GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
-  
-  //Make it more like a watch face
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  
-  //Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
-}
-
-static void main_window_unload(Window *window){
-  //Destroy the text layer
-  text_layer_destroy(s_time_layer);
-}
-
-static void init(){
-  //Create main window element and assign it to the pointer
-  s_main_window = window_create();
-  
-  //Set handlers to manage the elements inside the window
-  window_set_window_handlers(s_main_window, (WindowHandlers){
-    .load = main_window_load,
-    .unload = main_window_unload
-  });
-  
-  //Show the window on the watach
-  window_stack_push(s_main_window, true);
-  
-  //Make sure the time is displayed from the start
-  update_time();
-  
-  //Register with TickTimerService
-  tick_timer_service_subscsribe(MINUTE_UNIT, tick_handler);
-}
-
-static void deinit(){
-  //Destroy window
-  window_destroy(s_main_window);
 }
 
 int main(void){
-  init();
-  app_event_loop();
-  deinit();
+  init(s_main_window);
+  app_event_loop();//Event loop, app will wait for system event
+  deinit(s_main_window);
+  
 }
